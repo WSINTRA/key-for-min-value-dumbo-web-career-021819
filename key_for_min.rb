35c2b7ab964fceec98d98370c020fd71d4fc9278
William Robert Simpson <will.simpson85@gmail.com>
2019-01-24 23:39:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  testcase = []
-name_hash.each do |x,y,d|
-  testcase += [x,y]
+def key_for_min_value(list)
+#  testcase = []
+#name_hash.collect do |x,y,d|
+groups = list.group_by { |n| n <=> list.first }
+less_than    = groups[-1] || []
+first        = groups[0]  || []
+greater_than = groups[1]  || []
+quick_sort(less_than) + first + quick_sort(greater_than)
 
-end
-puts testcase
+  
 end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}

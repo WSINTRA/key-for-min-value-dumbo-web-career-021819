c9fc425da4d20234b2eff5b96063db7a8afda15a
William Robert Simpson <will.simpson85@gmail.com>
2019-01-24 23:26:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  testcase = []
 name_hash.each do |x,y,d|
   testcase = [x,y,d]
-  puts testcase
-end
 
+end
+testcase
 end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}

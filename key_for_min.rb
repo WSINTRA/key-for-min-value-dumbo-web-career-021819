cb85229946ab7f070cc67299fb1ac74c78e95316
William Robert Simpson <will.simpson85@gmail.com>
2019-01-24 23:50:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(list)
-  testcase = 0
+  testcase = 999
 list.each do |x,y|
-    testcase = y
   if y < testcase
     testcase = y
   end
